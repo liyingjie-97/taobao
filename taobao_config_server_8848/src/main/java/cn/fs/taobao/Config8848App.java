3f5ac1828f745a1eb55873f9a2334a681c850a63
lyj <lyj@qq.com>
2019-05-06 22:19:09 +0800

    测试

--- taobao_config_server_8848/src/main/java/cn/fs/taobao/Config8848App.java
+++ taobao_config_server_8848/src/main/java/cn/fs/taobao/Config8848App.java
@@ -2,10 +2,12 @@ package cn.fs.taobao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigServer//启用配置服务端
 public class Config8848App {
 
     public static void main(String[] args) {
